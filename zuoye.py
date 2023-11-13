73b87d2a508f2f0adec3e46bf1ce2dec8a953674
Songzeyu1016 <150645353+Songzeyu1016@users.noreply.github.com>
2023-11-13 13:02:32 +0800

    Add files via upload

---
+++ zuoye.py
@@ -0,0 +1,10 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> score = int(input('输入分数:\n'))
+... if score >= 90:
+...     grade = 'A'
+... elif score >= 60:
+...     grade = 'B'
+... else:
+...     grade = 'C'
+...  
